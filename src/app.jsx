b89060c60d06ac95bdd3bb51b1e933957b47f6b2
John Wu <john.wu@mail.mcgill.ca>
2018-06-07 00:09:17 -0700

    Starting cover page

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Header from './components/header.jsx'
 
 class App extends React.Component {
   render() {
     return (
-      <div>Hello World!</div>
+      <Header></Header>
     );
   }
 }
---
+++ src/components/header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+        <div class="header">
+            <div class="container">
+                <h2>JOHN WU</h2>
+                <p>I LIKE GOING ON ADVENTURES, COOKING SPICY FOOD, AND BUILDING COOL SHIT.</p>
+            </div>
+        </div>
+    );
+  }
+}
+
+export default Header;
